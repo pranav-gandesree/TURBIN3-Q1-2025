b1a65dab81ebb37fbcaafc99be4e86ac027488b6
pranav-gandesree <pranav8267@gmail.com>
2025-02-04 18:46:03 +0530

    nft marketplace

---
+++ nft-marketplace/programs/marketplace/src/state/marketplace.rs
@@ -0,0 +1,17 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Marketplace{
+    pub admin: Pubkey,
+    pub fee: u16, 
+
+    pub bump: u8,
+    pub treasury_bump: u8,
+    pub rewards_bump: u8,
+
+    pub name: String //set the limit to 32 bytes
+}
+
+impl Space for Marketplace {
+    const INIT_SPACE: usize = 8 + 32 + 2 + 3 + (4 + 32);
+}
